bf18cdfcf76dbf256e0b8c1a1057e0267e50f968
Anilkumar Leishangthem <anilkumarleishangthem6@gmail.com>
2026-03-23 14:55:22 +0530

    Add files via upload
    flies of Task13 uploaded successfully

---
+++ Task13/README.md
@@ -0,0 +1,3 @@
+Odd Even check program.
+
+In this program, we use if-else statements to check if the given number is an even or not.
---
+++ Task13/script.js
@@ -0,0 +1,33 @@
+let a = 121;
+let b = 30;
+let c = 67;
+let d = 50;
+
+if(a % 2 == 0){
+  console.log("The number "+a+" is even");
+}
+else{
+  console.log("The number "+a+" is odd");
+}
+
+if(b % 2 == 0){
+  console.log("The number "+b+" is even");
+}
+else{
+  console.log("The number "+b+" is odd");
+}
+
+if(c % 2 == 0){
+  console.log("The number "+c+" is even");
+}
+else{
+  console.log("The number "+c+" is odd");
+}
+
+if(d % 2 == 0){
+  console.log("The number "+d+" is even");
+}
+else{
+  console.log("The number "+d+" is odd");
+}
+
